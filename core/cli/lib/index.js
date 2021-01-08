035b1f811f2ec5e9ba5b5b53cb9fc898ba495b6b
EditYJ <158392613@qq.com>
2021-01-08 15:50:19 +0800

    更改项目结构，引入import-local确保优先调用本地版本

---
+++ core/cli/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = cli;
+
+function cli(argv) {
+  console.log("传入的参数是", argv);
+}
--- packages/@xdjx/cli/bin/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const utils = require("@xdjx/cli-utils");
-
-utils();
-console.log("hello xdjx-cli 我是脚手架入口包!");
